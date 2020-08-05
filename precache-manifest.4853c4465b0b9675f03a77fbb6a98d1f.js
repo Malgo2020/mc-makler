a74555d2ad203ff0e6a45438297ba65cae13a30c
MalgoBlock <mmmzemla@gmail.com>
2020-08-05 10:21:25 +0200

    Updates

---
+++ precache-manifest.4853c4465b0b9675f03a77fbb6a98d1f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883fef60d3f83558a7e2fe5c7a1737bc",
+    "url": "/mc-makler/index.html"
+  },
+  {
+    "revision": "ebab82750474bb5a12d7",
+    "url": "/mc-makler/static/css/main.c92a8c5b.chunk.css"
+  },
+  {
+    "revision": "3d651cb78e1287d5b530",
+    "url": "/mc-makler/static/js/2.ce1b7c73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mc-makler/static/js/2.ce1b7c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebab82750474bb5a12d7",
+    "url": "/mc-makler/static/js/main.35832635.chunk.js"
+  },
+  {
+    "revision": "376e9dd5325e6431836e",
+    "url": "/mc-makler/static/js/runtime-main.9caf8e88.js"
+  },
+  {
+    "revision": "8317e19779d654ef5b47da06542cdbe3",
+    "url": "/mc-makler/static/media/active.8317e197.svg"
+  },
+  {
+    "revision": "61697c12cba4236a3f990bc7f929d6c1",
+    "url": "/mc-makler/static/media/arrow_drop_down.61697c12.svg"
+  },
+  {
+    "revision": "915883cff37a538068c312eb8f54a131",
+    "url": "/mc-makler/static/media/back.915883cf.svg"
+  },
+  {
+    "revision": "c49dede7f08bfa5c2b2dbe63df6eda2e",
+    "url": "/mc-makler/static/media/ic-contact_support.c49dede7.svg"
+  },
+  {
+    "revision": "2e2cc9721cfc99f3a3ab9d8784f9f1bb",
+    "url": "/mc-makler/static/media/ic-menu_link.2e2cc972.svg"
+  },
+  {
+    "revision": "b3f406a69afd2eb3944a3ce1b433d2a0",
+    "url": "/mc-makler/static/media/ic-power_settings_new.b3f406a6.svg"
+  },
+  {
+    "revision": "d5e268c5f423fdba1014a3125370fa78",
+    "url": "/mc-makler/static/media/ic-user.d5e268c5.svg"
+  },
+  {
+    "revision": "391137b062db2e14563ba94548a978ac",
+    "url": "/mc-makler/static/media/logo.391137b0.svg"
+  },
+  {
+    "revision": "3933dade26a59730e6246f360712565f",
+    "url": "/mc-makler/static/media/search.3933dade.svg"
+  }
+]);
